c4d5cf09c616f1e87c5e1b046fbd8e547bdd05dc
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-04-11 09:04:36 +0000

    Created working FetchFTPFile step
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@5185 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

---
+++ pentaho-plugins/FetchFTPFile32/src/org/kaltura/fetchftpfile/FTPHelper.java
@@ -0,0 +1,63 @@
+package org.kaltura.fetchftpfile;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import com.enterprisedt.net.ftp.FTPClient;
+import com.enterprisedt.net.ftp.FTPConnectMode;
+import com.enterprisedt.net.ftp.FTPException;
+import com.enterprisedt.net.ftp.FTPTransferType;
+
+public class FTPHelper
+{
+	public static FTPClient connectToFTP(String host, int port, String user, String pw, boolean activeMode, boolean binaryMode, int timeout, String encoding) throws IOException, FTPException
+    {
+		FTPClient ftpclient;
+
+		 // Create ftp client to host:port ...
+        ftpclient = new FTPClient();
+        
+        ftpclient.setRemoteAddr(InetAddress.getByName(host));
+        ftpclient.setRemotePort(port);
+        
+        ftpclient.setTimeout(timeout);
+        ftpclient.setControlEncoding(encoding);
+        ftpclient.setConnectMode(activeMode ? FTPConnectMode.ACTIVE : FTPConnectMode.PASV);
+        
+        // login to ftp host ...
+        ftpclient.connect();     
+        
+        // login now ...
+        ftpclient.login(user, pw);
+
+        ftpclient.setType(binaryMode ? FTPTransferType.BINARY : FTPTransferType.ASCII);
+        
+        return ftpclient;
+    }
+	
+	public static boolean GetFTPFile(FTPClient ftpClient, String remotePath, String localDir, String localFileName)
+	{
+		if (ftpClient == null || !ftpClient.connected())
+		{
+			return false;
+		}
+		
+		try
+		{
+			ftpClient.get(localDir + "/" + localFileName, remotePath);
+		}
+		catch (Exception ex)
+		{
+			if (ex instanceof FTPException)
+			{
+				// TODO : Handle
+			}
+			else if (ex instanceof IOException)
+			{
+				// TODO: Handle
+			}
+			return false;
+		}
+		return true;
+	}
+}
