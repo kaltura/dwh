6acb649bc7d189df9f3f7c9b44f95a0cfd43a0af
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-05-12 08:25:13 +0000

    Removed scheduler and scheduler_worker
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@5346 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/6011_batch_job/20110511_add_pentaho_sequences_records.sql
+++ ddl/migrations/development/6011_batch_job/20110511_add_pentaho_sequences_records.sql
@@ -1,1 +1,1 @@
-INSERT INTO kalturadw_ds.pentaho_sequences VALUES(9,'dimensions/update_scheduler.ktr',1,TRUE),(9,'dimensions/update_scheduler_worker.ktr',2,TRUE),(9,'dimensions/update_batch_job.ktr',3,TRUE);
+INSERT INTO kalturadw_ds.pentaho_sequences VALUES(9,'dimensions/update_batch_job.ktr',1,TRUE);
