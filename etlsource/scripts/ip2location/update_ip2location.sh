1b6e644b8f0b4d0eea865311883662fe3d78b9ac
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-05-26 11:39:06 +0000

    Updated the script to access parameters for the root folder of the code, and a temp folder
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@5488 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- etlsource/scripts/ip2location/update_ip2location.sh
+++ etlsource/scripts/ip2location/update_ip2location.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-mkdir `date +%b%y`
-cd `date +%b%y`
-perl ../download.pl -package DB7 -login alex.bandel@kaltura.com -password S47CW89L
+
+KITCHEN=/usr/local/pentaho/pdi/kitchen.sh
+ROOT_DIR=/opt/kaltura/dwh
+TMP_DIR=/tmp
+
+while getopts "k:p:" o
+do      case "$o" in
+    k)  KITCHEN="$OPTARG";;
+    p)  ROOT_DIR="$OPTARG";;
+        [?])    echo >&2 "Usage: $0 [-k  pdi-path] [-p dwh-path]"
+                exit 1;;
+        esac
+done
+
+
+mkdir -p $ROOT_DIR/etlsource/scripts/ip2location/`date +%b%y`
+cd $ROOT_DIR/etlsource/scripts/ip2location/`date +%b%y`
+perl $ROOT_DIR/etlsource/scripts/ip2location/download.pl -package DB7 -login alex.bandel@kaltura.com -password S47CW89L
 
 unzip IP-COUNTRY-REGION-CITY-ISP-DOMAIN-FULL.ZIP
 
-mv IP-COUNTRY-REGION-CITY-ISP-DOMAIN.CSV /tmp/IP-COUNTRY-REGION-CITY-ISP-DOMAIN.CSV
+mv IP-COUNTRY-REGION-CITY-ISP-DOMAIN.CSV $TMP_DIR/IP-COUNTRY-REGION-CITY-ISP-DOMAIN.CSV
 
-/usr/local/pentaho/pdi/kitchen.sh -file /home/etl/etlsource/ip2location/load_ip2location.kjb
+$KITCHEN -file $ROOT_DIR/etlsource/ip2location/load_ip2location.kjb
