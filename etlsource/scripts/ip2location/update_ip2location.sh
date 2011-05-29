7cd7587a710fa3f7854288f9dcbb21b870f25978
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-05-29 07:43:06 +0000

    1. Updated the script to pass the tmp dir in a parameter to the pentaho job. 2. Updated the script load the data from a file located on the server that runs the ETL and not the DWH server
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@5503 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- etlsource/scripts/ip2location/update_ip2location.sh
+++ etlsource/scripts/ip2location/update_ip2location.sh
@@ -2,13 +2,14 @@
 
 KITCHEN=/usr/local/pentaho/pdi/kitchen.sh
 ROOT_DIR=/opt/kaltura/dwh
-TMP_DIR=/tmp
+TMP_DIR=/tmp/
 
 while getopts "k:p:" o
 do      case "$o" in
     k)  KITCHEN="$OPTARG";;
     p)  ROOT_DIR="$OPTARG";;
-        [?])    echo >&2 "Usage: $0 [-k  pdi-path] [-p dwh-path]"
+    t)  TMP_DIR="$OPTARG";;
+        [?])    echo >&2 "Usage: $0 [-k  pdi-path] [-p dwh-path] [-t tmp_dir]"
                 exit 1;;
         esac
 done
@@ -22,4 +23,4 @@ unzip IP-COUNTRY-REGION-CITY-ISP-DOMAIN-FULL.ZIP
 
 mv IP-COUNTRY-REGION-CITY-ISP-DOMAIN.CSV $TMP_DIR/IP-COUNTRY-REGION-CITY-ISP-DOMAIN.CSV
 
-$KITCHEN -file $ROOT_DIR/etlsource/ip2location/load_ip2location.kjb
+$KITCHEN -file $ROOT_DIR/etlsource/ip2location/load_ip2location.kjb -param:ExportPath=$TMP_DIR
