cec36f675aac604eb32378c201977d5fda5f4c4f
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-05-30 13:18:51 +0000

    Created main monitoring script. It was written by Alex
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@5521 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

---
+++ monitoring/etlmon.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+MONDIR=/opt/kaltura/dwh/monitoring
+COLOR=green
+#BBTMP=/home/xymon/client/tmp
+OUTFILE=$BBTMP/outfile.$$
+#BB=/home/xymon/client/bin/bb
+#BBDISP="208.122.58.141"
+MACHINE=`hostname`
+for mycheck in `ls $MONDIR | grep ^find`
+    do
+      ##echo "$mycheck running :"
+      outcheck=`mysql -uetl -petl < $MONDIR/$mycheck`
+      if [ "X$outcheck" != "X" ]
+         then
+            echo "Error in $mycheck query : "  >> $OUTFILE
+            echo "$outcheck" >> $OUTFILE
+            echo -e "\n" >> $OUTFILE
+            if [ $COLOR == "green" ] ; then
+                 COLOR=red
+            fi
+            if [ $mycheck == "find_ri_new_rows.sql" ] ; then
+                COLOR=yellow
+            fi
+         fi
+    done
+#COLOR=green
+if [ $COLOR == "green" ]; then
+     echo "All checks are ok " >> $OUTFILE
+fi
+$BB $BBDISP "status $MACHINE.etlmon $COLOR `date`
+
+`cat $OUTFILE`
+"
+
+rm -f $OUTFILE
