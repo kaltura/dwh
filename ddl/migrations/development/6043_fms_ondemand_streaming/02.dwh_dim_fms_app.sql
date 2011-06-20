af4484392e50cea0199a56ab5aa1dbc29c8c11a7
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-06-20 10:54:14 +0000

    Added another process for fms_ondemand_streaming. This caused the change of the fms_streaming current process to be fms_live_streaming.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@5674 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

---
+++ ddl/migrations/development/6043_fms_ondemand_streaming/02.dwh_dim_fms_app.sql
@@ -0,0 +1,6 @@
+ALTER TABLE kalturadw.dwh_dim_fms_app CHANGE app_id  fms_app_id INT(11) unsigned, CHANGE app fms_app_name varchar(45);
+  
+ALTER TABLE kalturadw.dwh_dim_fms_app
+  add `dwh_creation_date` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+  add `dwh_update_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  add `ri_ind` TINYINT(4) NOT NULL DEFAULT '1';
