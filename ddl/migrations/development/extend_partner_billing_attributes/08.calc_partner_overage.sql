24687c38f87f5b0fd1d224d18072130dccef0f95
omerg <omerg@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-08-14 11:50:06 +0000

    fwr: Fixes to overage calculation and added billing report (need to test the report when data is available)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6172 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/extend_partner_billing_attributes/08.calc_partner_overage.sql
+++ ddl/migrations/development/extend_partner_billing_attributes/08.calc_partner_overage.sql
@@ -26,7 +26,11 @@ BEGIN
 			q.charge_monthly_entries_usd,
 			q.charge_monthly_entries_unit
 		FROM kalturadw.dwh_dim_partners q LEFT OUTER JOIN kalturadw.dwh_dim_partners p ON (q.partner_id = p.partner_parent_id AND q.partner_group_type_id = 3)
-		WHERE (q.max_monthly_bandwidth_kb IS NOT NULL AND q.charge_monthly_bandwidth_kb_usd IS NOT NULL AND q.charge_monthly_bandwidth_kb_unit IS NOT NULL);
+		WHERE (q.max_monthly_bandwidth_kb IS NOT NULL AND q.charge_monthly_bandwidth_kb_usd IS NOT NULL AND q.charge_monthly_bandwidth_kb_unit IS NOT NULL)
+		OR (q.max_monthly_storage_mb IS NOT NULL AND q.charge_monthly_storage_mb_usd IS NOT NULL AND q.charge_monthly_storage_mb_unit IS NOT NULL)
+		OR (q.max_monthly_total_usage_mb IS NOT NULL AND q.charge_monthly_total_usage_mb_usd IS NOT NULL AND q.charge_monthly_total_usage_mb_unit IS NOT NULL)
+		OR (q.max_monthly_entries IS NOT NULL AND q.charge_monthly_entries_usd IS NOT NULL AND q.charge_monthly_entries_unit IS NOT NULL)
+		OR (q.max_monthly_plays IS NOT NULL AND q.charge_monthly_plays_usd IS NOT NULL AND q.charge_monthly_plays_unit IS NOT NULL);
 	
 		DROP TABLE IF EXISTS partner_overages;
 		CREATE TEMPORARY TABLE partner_overages (
@@ -54,45 +58,45 @@ BEGIN
 				MAX(max_monthly_bandwidth_kb) included_bandwidth_kb,
 				@BANDWIDTH:=IFNULL(SUM(count_bandwidth_kb),0) actual_bandwidth_kb,
 				get_overage_charge(MAX(max_monthly_bandwidth_kb), @BANDWIDTH, MAX(charge_monthly_bandwidth_kb_unit), MAX(charge_monthly_bandwidth_kb_usd)) charge_overage_bandwidth_kb,
-				MAX(charge_monthly_storage_mb_unit) included_storage_mb,
+				MAX(max_monthly_storage_mb) included_storage_mb,
 				@STORAGE:=calc_partner_monthly_storage(p_month_id , pq.partner_id) actual_storage_mb,
 				get_overage_charge(MAX(max_monthly_storage_mb), @STORAGE, MAX(charge_monthly_storage_mb_unit), MAX(charge_monthly_storage_mb_usd)) charge_overage_storage_mb, 
-				MAX(charge_monthly_total_usage_mb_unit) included_total_usage_mb,
+				MAX(max_monthly_total_usage_mb) included_total_usage_mb,
 				@BANDWIDTH + @STORAGE actual_total_usage_mb,
 				get_overage_charge(MAX(max_monthly_total_usage_mb), @BANDWIDTH + @STORAGE, MAX(charge_monthly_total_usage_mb_unit), MAX(charge_monthly_total_usage_mb_usd)) charge_overage_total_usage_mb
 			FROM partner_quotas pq LEFT OUTER JOIN kalturadw.dwh_hourly_partner_usage partner_usage 
-					ON (pq.usage_partner_id = partner_usage.partner_id AND partner_usage.date_id BETWEEN DATE(p_month_id*100+1)*1 AND LAST_DAY(DATE(p_month_id*100+1))*1)
+					ON (pq.usage_partner_id = partner_usage.partner_id AND partner_usage.date_id BETWEEN p_month_id*100 AND p_month_id*100+31)
 			GROUP BY pq.partner_id
-			HAVING 	@BANDWIDTH > MAX(max_monthly_bandwidth_kb) OR 
-				@STORAGE > MAX(max_monthly_bandwidth_kb) OR 
-				(@BANDWIDTH+@STORAGE) > MAX(max_monthly_bandwidth_kb)
+			HAVING 	actual_bandwidth_kb > included_bandwidth_kb OR 
+				actual_storage_mb > included_storage_mb OR 
+				(actual_bandwidth_kb+actual_storage_mb) > included_total_usage_mb
 		ON DUPLICATE KEY UPDATE 
 			included_bandwidth_kb=VALUES(included_bandwidth_kb),
 			actual_bandwidth_kb=VALUES(actual_bandwidth_kb),
 			charge_overage_bandwidth_kb=VALUES(charge_overage_bandwidth_kb),
 			included_storage_mb=VALUES(included_storage_mb),
+			actual_storage_mb=VALUES(actual_storage_mb),
 			charge_overage_storage_mb=VALUES(charge_overage_storage_mb),
-			charge_overage_bandwidth_kb=VALUES(charge_overage_bandwidth_kb),
 			included_total_usage_mb=VALUES(included_total_usage_mb),
 			actual_total_usage_mb=VALUES(actual_total_usage_mb),
 			charge_overage_total_usage_mb=VALUES(charge_overage_total_usage_mb);
+			
 		INSERT INTO partner_overages (partner_id, included_entries, actual_entries, charge_overage_entries)
 			SELECT 	pq.partner_id, 
 				MAX(max_monthly_entries) included_entries,
-				COUNT(*) actual_entries,
-				get_overage_charge(MAX(max_monthly_entries), COUNT(*), MAX(charge_monthly_entries_unit), MAX(charge_monthly_entries_usd)) charge_overage_entries
+				COUNT(entries.entry_id) actual_entries,
+				get_overage_charge(MAX(max_monthly_entries), COUNT(entries.entry_id), MAX(charge_monthly_entries_unit), MAX(charge_monthly_entries_usd)) charge_overage_entries
 			FROM partner_quotas pq, kalturadw.dwh_dim_entries entries
 			WHERE 	pq.usage_partner_id = entries.partner_id AND 
 				entries.created_at < LAST_DAY(DATE(p_month_id*100+1)) + INTERVAL 1 DAY AND
 				entries.entry_type_id IN (1,7) /*Media and Live streams*/
 				AND entry_status_id NOT IN (-2,-1,3) /*Remove failed and deleted*/
 			GROUP BY pq.partner_id
-			HAVING COUNT(*) > MAX(max_monthly_entries)
+			HAVING actual_entries > included_entries
 		ON DUPLICATE KEY UPDATE 
 			included_entries=VALUES(included_entries),
 			actual_entries=VALUES(actual_entries),
 			charge_overage_entries=VALUES(charge_overage_entries);
-	
 	
 		INSERT INTO partner_overages (partner_id, included_plays, actual_plays, charge_overage_plays)
 			SELECT 	pq.partner_id, 
@@ -101,7 +105,7 @@ BEGIN
 				get_overage_charge(MAX(max_monthly_plays), SUM(count_plays), MAX(charge_monthly_plays_unit), MAX(charge_monthly_plays_usd)) charge_overage_plays
 			FROM partner_quotas pq, kalturadw.dwh_hourly_partner plays
 			WHERE 	pq.usage_partner_id = plays.partner_id AND
-				date_id BETWEEN DATE(p_month_id*100+1)*1 AND LAST_DAY(DATE(p_month_id*100+1))*1
+				date_id BETWEEN p_month_id*100 AND p_month_id*100+31
 			GROUP BY pq.partner_id
 			HAVING 	SUM(count_plays) > MAX(max_monthly_plays)
 		ON DUPLICATE KEY UPDATE 
@@ -114,7 +118,6 @@ BEGIN
 			group_types.partner_group_type_name,
 			IF(children.partner_group_type_id=3,NULL,children.partner_id) publisher_id,
 			IF(children.partner_group_type_id=3,NULL,children.partner_name) publisher_name,
-			IF(children.partner_group_type_id=3,children.class_of_service_id,parents.partner_id) parent_partner_id,
 			d_cos.partner_class_of_service_name,
 			d_vertical.partner_vertical_name,
 			included_bandwidth_kb,
