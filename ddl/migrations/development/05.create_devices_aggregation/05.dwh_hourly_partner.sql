73672b25c37fcdfb22d9b3c9bfdc8844c993e803
omerg <omerg@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-08-25 06:46:37 +0000

    fwr: added deleted and new media to OLAP aggregations
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6284 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/05.create_devices_aggregation/05.dwh_hourly_partner.sql
+++ ddl/migrations/development/05.create_devices_aggregation/05.dwh_hourly_partner.sql
@@ -1,1 +1,4 @@
-ALTER TABLE `kalturadw`.`dwh_hourly_partner` ADD COLUMN count_admins INT;
+ALTER TABLE `kalturadw`.`dwh_hourly_partner` 
+ADD COLUMN new_admins INT,
+ADD COLUMN new_media_entries INT,
+ADD COLUMN deleted_media_entries INT;
--- ddl/migrations/development/05.create_devices_aggregation/07.post_aggregation_partner.sql
+++ ddl/migrations/development/05.create_devices_aggregation/07.post_aggregation_partner.sql
@@ -11,6 +11,7 @@ BEGIN
 	SELECT aggr_table INTO v_aggr_table
 	FROM kalturadw_ds.aggr_name_resolver
 	WHERE aggr_name = 'partner';
+	
 	SET @s = CONCAT('INSERT INTO ',v_aggr_table,'
     		(partner_id, 
     		date_id, 
@@ -19,7 +20,10 @@ BEGIN
     		count_image, 
     		count_audio, 
     		count_mix,
-    		count_playlist)
+    		count_playlist,
+			new_media_entries,
+			deleted_media_entries			
+			)
     	SELECT  
     		partner_id,date_id,hour_id,
     		SUM(count_video) sum_count_video,
@@ -27,15 +31,17 @@ BEGIN
     		SUM(count_audio) sum_count_audio,
     		SUM(count_mix) sum_count_mix,
     		SUM(count_playlist) sum_playlist
+			SUM(new_media_entries) new_media_entries
     	FROM (
     		SELECT partner_id,en.created_date_id date_id,HOUR(en.created_at) hour_id,
     			COUNT(IF(entry_media_type_id = 1, 1,NULL)) count_video,
     			COUNT(IF(entry_media_type_id = 2, 1,NULL)) count_image,
     			COUNT(IF(entry_media_type_id = 5, 1,NULL)) count_audio,
     			COUNT(IF(entry_media_type_id = 6, 1,NULL)) count_mix,
-    			COUNT(IF(entry_type_id = 5, 1,NULL)) count_playlist
+    			COUNT(IF(entry_type_id = 5, 1,NULL)) count_playlist,
+				COUNT(IF(entry_type_id IN (1,7), 1,NULL)) new_media_entries
     		FROM dwh_dim_entries  en 
-    		WHERE (en.entry_media_type_id IN (1,2,5,6) OR en.entry_type_id IN (5) ) 
+    		WHERE (en.entry_media_type_id IN (1,2,5,6) OR en.entry_type_id IN (1, 5, 7) ) 
     			AND en.created_at between DATE(''',date_val,''') + INTERVAL ', p_hour_id, ' HOUR ',' 
 					   AND DATE(''',date_val,''') + INTERVAL ', p_hour_id, ' + 1 HOUR ','
     		GROUP BY partner_id, en.created_date_id, en.created_hour_id
@@ -46,7 +52,34 @@ BEGIN
     		count_image=VALUES(count_image),
     		count_audio=VALUES(count_audio),
     		count_mix=VALUES(count_mix),
-    		count_playlist=VALUES(count_playlist);
+    		count_playlist=VALUES(count_playlist),
+			new_media_entries=VALUES(new_media_entries);
+    	');
+	PREPARE stmt FROM  @s;
+	EXECUTE stmt;
+	DEALLOCATE PREPARE stmt;
+	
+	SET @s = CONCAT('INSERT INTO ',v_aggr_table,'
+    		(partner_id, 
+    		date_id, 
+            hour_id,
+			deleted_media_entries			
+			)
+    	SELECT  
+    		partner_id,date_id,hour_id,
+			SUM(deleted_media_entries) deleted_media_entries
+    	FROM (
+    		SELECT partner_id,en.created_date_id date_id,HOUR(en.created_at) hour_id,
+				COUNT(1) deleted_media_entries
+    		FROM dwh_dim_entries  en 
+    		WHERE (en.entry_media_type_id IN (1,2,5,6) OR en.entry_type_id IN (5) and entry_status_id = 3) 
+    			AND en.modified_at between DATE(''',date_val,''') + INTERVAL ', p_hour_id, ' HOUR ',' 
+					   AND DATE(''',date_val,''') + INTERVAL ', p_hour_id, ' + 1 HOUR ','
+    		GROUP BY partner_id, en.created_date_id, en.created_hour_id
+    	) AS a
+    	GROUP BY partner_id,date_id, hour_id
+    	ON DUPLICATE KEY UPDATE
+    		deleted_media_entries=VALUES(deleted_media_entries);
     	');
 	PREPARE stmt FROM  @s;
 	EXECUTE stmt;
@@ -57,18 +90,18 @@ BEGIN
     		(partner_id, 
     		date_id, 
             hour_id,
-    		count_users, count_admins)
+    		count_users, new_admins)
     	SELECT  
     		partner_id,ku.created_date_id, ku.created_hour_id,
     		COUNT(1) count_users,
-			SUM(IF(is_admin=1,1,NULL)) count_admins
+			SUM(IF(is_admin=1,1,NULL)) new_admins
     	FROM dwh_dim_kusers  ku
     	WHERE ku.created_at between DATE(''',date_val,''') + INTERVAL ', p_hour_id, ' HOUR ',' 
 					   AND DATE(''',date_val,''') + INTERVAL ', p_hour_id, ' + 1 HOUR ','
    		GROUP BY partner_id,ku.created_date_id, ku.created_hour_id
     	ON DUPLICATE KEY UPDATE
     		count_users=VALUES(count_users) ,
-			count_admins=VALUES(count_admins) ;
+			new_admins=VALUES(new_admins) ;
         ');
 	
 	PREPARE stmt FROM  @s;
