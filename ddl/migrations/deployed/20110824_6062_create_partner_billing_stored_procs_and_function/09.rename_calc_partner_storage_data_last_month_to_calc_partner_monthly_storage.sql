30ff72158ff914190148012f44e94c5a3c79ced4
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-08-29 11:45:52 +0000

    qnd: Hotfix to Eagle branch calc_partner_monthly_storage
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6311 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/deployed/20110824_6062_create_partner_billing_stored_procs_and_function/09.rename_calc_partner_storage_data_last_month_to_calc_partner_monthly_storage.sql
+++ ddl/migrations/deployed/20110824_6062_create_partner_billing_stored_procs_and_function/09.rename_calc_partner_storage_data_last_month_to_calc_partner_monthly_storage.sql
@@ -9,7 +9,7 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `calc_partner_monthly_storage`(p_mont
 BEGIN
 	DECLARE avg_cont_aggr_storage DECIMAL(19,4);
 
-        SELECT  calc_partner_storage_data_time_range (date(p_month_id*100+1),last_day(p_month_id*100+1),p_partner_id)
+        SELECT  calc_partner_storage_data_time_range (date(p_month_id*100+1)*1,last_day(p_month_id*100+1)*1,p_partner_id)
 	INTO avg_cont_aggr_storage;
         RETURN avg_cont_aggr_storage;
 END$$
