0d6a3919a78348c27394b2233b5fcf7225e2b947
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-08-31 11:03:01 +0000

    qnd: fixed monitoring script
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6349 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- monitoring/find_aggr_did_not_run.sql
+++ monitoring/find_aggr_did_not_run.sql
@@ -1,8 +1,6 @@
 /*Find aggregations in aggr_managment that cover the time frame of the day before yesterday which didn't run yet*/
-SELECT aggr_name, 'Aggregate did not run' stat, DATE(NOW())-INTERVAL 2 DAY DATE
--- DATE(NOW())-INTERVAL IF(aggr_name='partner_usage',3,2) DAY
+SELECT aggr_name, 'Aggregate did not run' stat, DATE(NOW())-INTERVAL IF(aggr_name='bandwidth_usage',3,2) DAY DATE
 FROM kalturadw.aggr_managment
-WHERE aggr_day_int = DATE(NOW())- INTERVAL 2 DAY
--- INTERVAL IF(aggr_name='partner_usage',3,2) DAY)*1
+WHERE aggr_day_int = (DATE(NOW())-INTERVAL IF(aggr_name='bandwidth_usage',3,2) DAY)*1
 AND is_calculated = 0;
 
