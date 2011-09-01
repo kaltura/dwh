473fdff30ad9c9101ead7d0ab9339a8a6065b820
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-09-01 06:57:29 +0000

    qnd: removed the main old monitoring script
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6356 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- monitoring/find_too_many_invalid_lines.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'too many invalid event lines' stat, COUNT(*) amount, (DATE(NOW())-INTERVAL 1 DAY)*1 DATE
-FROM kalturadw_ds.invalid_event_lines
-WHERE date_id = (DATE(NOW())-INTERVAL 1 DAY)*1
-HAVING COUNT(*) > 100000
-UNION ALL 
-SELECT 'too many invalid fms lines' stat, COUNT(*) amount, (DATE(NOW())-INTERVAL 1 DAY)*1 DATE
-FROM kalturadw_ds.invalid_fms_event_lines
-WHERE date_id = (DATE(NOW())-INTERVAL 1 DAY)*1
-HAVING COUNT(*) > 100000;
