6e6d59d5984a3d05dcff682b4714bc7e456f0c12
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-09-08 13:43:32 +0000

    def: 1 added fixes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6405 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/move_operational_sync_to_pull/07.operational_syncs.sql
+++ ddl/migrations/development/move_operational_sync_to_pull/07.operational_syncs.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS operational_syncs;
 
 CREATE TABLE operational_syncs (
 	operational_sync_id INT(11),
-	operational_sync_name VARCHAR(50),
-	group_column VARCHAR(50),
-	entity_table VARCHAR(50),
-	aggregation_phrase VARCHAR(50),
-	aggregation_table VARCHAR(50),
-	bridge_entity VARCHAR(50),
-	bridge_table VARCHAR(50),
+	operational_sync_name VARCHAR(1024),
+	group_column VARCHAR(1024),
+	entity_table VARCHAR(1024),
+	aggregation_phrase VARCHAR(1024),
+	aggregation_table VARCHAR(1024),
+	bridge_entity VARCHAR(1024),
+	bridge_table VARCHAR(1024),
 	last_execution_parameter_id INT,	
 	execution_start_time_parameter_id INT,
 	PRIMARY KEY (operational_sync_id)
@@ -20,7 +20,7 @@ INSERT INTO operational_syncs
 	(operational_sync_id, operational_sync_name, group_column, entity_table, aggregation_phrase, aggregation_table, 
 	bridge_entity, bridge_table, last_execution_parameter_id, execution_start_time_parameter_id)
 	VALUES
-	(1, 'entry', 'entry_id', 'kalturadw.dwh_dim_entries', 'sum(count_plays) plays, sum(count_loads) views', 'kalturadw.dwh_hourly_events_entry', NULL, NULL, 4, 5),
-	(2, 'kuser', 'kuser_id', 'kalturadw.dwh_dim_kusers', 'sum(entry_additional_size_kb)', 'kalturadw.dwh_fact_entries_sizes', 'entry_id', 'kalturadw.dwh_dim_entries', 6, 7);
+	(1, 'entry', 'entry_id', 'kalturadw.dwh_dim_entries', 'ifnull(sum(count_plays), 0) plays, ifnull(sum(count_loads), 0) views', 'kalturadw.dwh_hourly_events_entry', NULL, NULL, 4, 5),
+	(2, 'kuser', 'kuser_id', 'kalturadw.dwh_dim_kusers', 'ifnull(sum(entry_additional_size_kb), 0) storage_size', 'kalturadw.dwh_fact_entries_sizes', 'entry_id', 'kalturadw.dwh_dim_entries', 6, 7);
 
 
