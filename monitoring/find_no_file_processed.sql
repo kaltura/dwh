fc2c09e213368259f99b485594d94d58c43d331f
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-09-12 10:08:02 +0000

    qnd: Updated monitoring scripts.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6415 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- monitoring/find_no_file_processed.sql
+++ monitoring/find_no_file_processed.sql
@@ -4,7 +4,7 @@ FROM (
 SELECT COUNT(*) amount
 FROM kalturadw_ds.files f, kalturadw_ds.cycles c WHERE c.process_id = 1
 AND f.cycle_id = c.cycle_id
-AND c.STATUS='DONE' AND f.insert_time > DATE(NOW())-INTERVAL 1 DAY) a
+AND c.STATUS='DONE' AND f.insert_time > NOW()-INTERVAL 12 HOUR) a
 WHERE amount = 0
 UNION
 SELECT 'No FMS file processed yesterday!' stat
@@ -12,7 +12,7 @@ FROM (
 SELECT COUNT(*) amount
 FROM kalturadw_ds.files f, kalturadw_ds.cycles c WHERE c.process_id = 2
 AND f.cycle_id = c.cycle_id
-AND c.STATUS='DONE' AND f.insert_time > DATE(NOW())-INTERVAL 1 DAY) a
+AND c.STATUS='DONE' AND f.insert_time > NOW()-INTERVAL 36 HOUR) a
 WHERE amount = 0
 UNION
 SELECT 'No Akamai BW file processed yesterday!' stat
@@ -20,7 +20,7 @@ FROM (
 SELECT COUNT(*) amount
 FROM kalturadw_ds.files f, kalturadw_ds.cycles c WHERE c.process_id = 4
 AND f.cycle_id = c.cycle_id
-AND c.STATUS='DONE' AND f.insert_time > DATE(NOW())-INTERVAL 1 DAY) a
+AND c.STATUS='DONE' AND f.insert_time > NOW()-INTERVAL 12 HOUR) a
 WHERE amount = 0
 UNION
 SELECT 'No LimeLight BW file processed yesterday!' stat
@@ -28,7 +28,7 @@ FROM (
 SELECT COUNT(*) amount
 FROM kalturadw_ds.files f, kalturadw_ds.cycles c WHERE c.process_id = 5
 AND f.cycle_id = c.cycle_id
-AND c.STATUS='DONE' AND f.insert_time > DATE(NOW())-INTERVAL 1 DAY) a
+AND c.STATUS='DONE' AND f.insert_time > NOW()-INTERVAL 36 HOUR) a
 WHERE amount = 0
 UNION
 SELECT 'No Level3 BW file processed yesterday!' stat
@@ -36,7 +36,7 @@ FROM (
 SELECT COUNT(*) amount
 FROM kalturadw_ds.files f, kalturadw_ds.cycles c WHERE c.process_id = 6
 AND f.cycle_id = c.cycle_id
-AND c.STATUS='DONE' AND f.insert_time > DATE(NOW())-INTERVAL 1 DAY) a
+AND c.STATUS='DONE' AND f.insert_time > NOW()-INTERVAL 36 HOUR) a
 WHERE amount = 0
 UNION
 SELECT 'No RTMP AKAMAI file processed yesterday!' stat
@@ -44,5 +44,5 @@ FROM (
 SELECT COUNT(*) amount
 FROM kalturadw_ds.files f, kalturadw_ds.cycles c WHERE c.process_id = 7
 AND f.cycle_id = c.cycle_id
-AND c.STATUS='DONE' AND f.insert_time > DATE(NOW())-INTERVAL 1 DAY) a
+AND c.STATUS='DONE' AND f.insert_time > NOW()-INTERVAL 12 HOUR) a
 WHERE amount = 0
