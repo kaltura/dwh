0250b3c611007fe91aa73b9ba979ef3bdb3a8e26
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-10-02 13:23:00 +0000

    fwr: resorted the dwh_dim_flavor_asset table according to the deleted_at column into a copy table
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6569 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/deployed/20110927_6077_split_flavor_asset_tags_and_remove_version/04.load_old_data.sql
+++ ddl/migrations/deployed/20110927_6077_split_flavor_asset_tags_and_remove_version/04.load_old_data.sql
@@ -1,66 +1,59 @@
-use kalturadw;
+USE kalturadw;
 
 DROP PROCEDURE IF EXISTS load_tags;
 
 DELIMITER $$
 
-CREATE PROCEDURE load_tags()
+CREATE DEFINER=`etl`@`localhost` PROCEDURE `load_tags`()
 BEGIN
-    DECLARE v_id INT;
+    DECLARE v_flavor_asset_id VARCHAR(60);
     DECLARE v_tags VARCHAR(256);
+    DECLARE v_updated_at TIMESTAMP;
     DECLARE v_tag_name VARCHAR(256);
-    DECLARE v_tag_id int;
+    DECLARE v_tag_id INT;
     DECLARE v_tags_done INT;
     DECLARE v_tags_idx INT;
     DECLARE done INT DEFAULT 0;
     DECLARE assets CURSOR FOR
-    SELECT id, tags 
+    SELECT id, tags, updated_at
     FROM dwh_dim_flavor_asset;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-    
+
     OPEN assets;
-    
+
     read_loop: LOOP
-        FETCH assets INTO v_id, v_tags;
+        FETCH assets INTO v_flavor_asset_id, v_tags, v_updated_at;
         IF done THEN
-			LEAVE read_loop;
-		END IF;
-        
-        set v_tags_done = 0;       
-        set v_tags_idx = 1;
+             LEAVE read_loop;
+	END IF;
 
-          while not v_tags_done do
+        SET v_tags_done = 0;
+        SET v_tags_idx = 1;
 
-            set v_tag_name = substring(v_tags, v_tags_idx, 
-            if(locate(',', v_tags, v_tags_idx) > 0, 
-                locate(',', v_tags, v_tags_idx) - v_tags_idx, 
-                length(v_tags)));
+        WHILE NOT v_tags_done DO
+            SET v_tag_name = SUBSTRING(v_tags, v_tags_idx,
+					IF(LOCATE(',', v_tags, v_tags_idx) > 0,
+					LOCATE(',', v_tags, v_tags_idx) - v_tags_idx,
+					LENGTH(v_tags)));
 
-            set v_tag_name = trim(v_tag_name);
-
-            if length(v_tag_name) > 0 then
-
-                set v_tags_idx = v_tags_idx + length(v_tag_name) + 1;
-
+            SET v_tag_name = TRIM(v_tag_name);
+            IF LENGTH(v_tag_name) > 0 THEN
+                SET v_tags_idx = v_tags_idx + LENGTH(v_tag_name) + 1;
                 -- add the tag if it doesnt already exist
-                insert ignore into dwh_dim_tags (tag_name) values (v_tag_name);
-
-                select tag_id into v_tag_id from dwh_dim_falvor_asset_tag where tag_name = v_tag_name;
+                INSERT IGNORE INTO dwh_dim_tags (tag_name) VALUES (v_tag_name);
+                
+		SELECT tag_id INTO v_tag_id FROM dwh_dim_tags WHERE tag_name = v_tag_name;
 
                 -- add the flavor_asset tag
-                insert ignore into dwh_dim_flavor_asset_tags (id, tag_id) values (v_id, v_tag_id);
-
-            else
-                set v_tags_done = 1;
-            end if;
-
-        end while;
-        
-        INSERT INTO dwh_dim_flavor_asset_tag;
+                INSERT IGNORE INTO dwh_dim_flavor_asset_tags (flavor_asset_id, tag_id, updated_at) VALUES (v_flavor_asset_id, v_tag_id, v_updated_at);
+            ELSE
+                SET v_tags_done = 1;
+            END IF;
+        END WHILE;
     END LOOP;
 END$$
 
-DELIMITER;
+DELIMITER ;
 
 CALL load_tags();
 
