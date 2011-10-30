909475256603b3c080bf477868d9f1ccb2e9280a
omerg <omerg@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-10-30 16:13:58 +0000

    qnd: deployed 6091 - delete mechanism to retention policy
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6762 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/ds/retention_policy.sql
+++ ddl/ds/retention_policy.sql
@@ -4,12 +4,13 @@ DROP TABLE IF EXISTS retention_policy;
 CREATE TABLE retention_policy (
 	table_name VARCHAR(256) NOT NULL,
 	archive_start_days_back INT DEFAULT 180,
-	archive_delete_days_back INT DEFAULT 365,
-	archive_last_partition DATE)
+	archive_delete_days_back INT DEFAULT 2000,
+	archive_last_partition DATE,
+    PRIMARY KEY (table_name))
 ENGINE=MYISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO retention_policy VALUES 
-('dwh_fact_events', 180, 365, DATE('2011-01-01')),
-('dwh_fact_bandwidth_usage', 180, 365, DATE('2011-01-01')),
-('dwh_fact_fms_session_events', 180, 365, DATE('2011-01-01')),
-('dwh_fact_fms_sessions', 180, 365, DATE('2011-01-01'));
+('dwh_fact_events', 180, 2000, DATE('2011-01-01')),
+('dwh_fact_bandwidth_usage', 180, 2000, DATE('2011-01-01')),
+('dwh_fact_fms_session_events', 180, 2000, DATE('2011-01-01')),
+('dwh_fact_fms_sessions', 180, 2000, DATE('2011-01-01'));
--- ddl/dw/maintenance/move_innodb_to_archive.sql
+++ ddl/dw/maintenance/move_innodb_to_archive.sql
@@ -6,71 +6,113 @@ DROP PROCEDURE IF EXISTS `move_innodb_to_archive`$$
 
 CREATE DEFINER=`etl`@`localhost` PROCEDURE `move_innodb_to_archive`()
 BEGIN
-	DECLARE v_partition VARCHAR(256);
-	DECLARE v_column VARCHAR(256);
-	DECLARE v_from_archive DATE;
-	DECLARE v_date_val INT;
 	DECLARE v_table_name VARCHAR(256);
-	DECLARE v_archive_name VARCHAR(256);
-	DECLARE v_exists INT DEFAULT 0;
+	DECLARE v_archive_name VARCHAR(256);	
+	DECLARE v_partition_name VARCHAR(256);
+	DECLARE v_partition_date_id INT;
+	DECLARE v_column VARCHAR(256);
+	DECLARE v_is_archived BOOL;
+	DECLARE v_is_in_fact BOOL;
+	
+	DECLARE v_drop_from_archive INT DEFAULT 0;
+	DECLARE v_drop_from_fact INT DEFAULT 0;
+	DECLARE v_migrate_from_fact INT DEFAULT 0;
+		
 	DECLARE done INT DEFAULT 0;
+	
 	DECLARE c_partitions 
-	CURSOR FOR 
-	SELECT partition_name, p.table_name, CONCAT(p.table_name,'_archive') archive_name, partition_expression column_name, DATE(partition_description)*1 date_val
+	CURSOR FOR 	
+	SELECT 	r.table_name, 
+		CONCAT(r.table_name, '_archive') archive_name,
+		partition_name, 
+		DATE(partition_description)*1 partition_date_id, 
+		partition_expression column_name,
+		MAX(IF(CONCAT(r.table_name, '_archive') = p.table_name,1,0)) is_archived, 
+		MAX(IF(r.table_name=p.table_name,1,0)) is_in_fact
 	FROM information_schema.PARTITIONS p, kalturadw_ds.retention_policy r
 	WHERE LENGTH(partition_description) = 8 
-    AND DATE(partition_description)*1 IS NOT NULL
-    AND partition_description <= DATE(NOW() - INTERVAL r.archive_start_days_back DAY)*1
-	AND p.table_name = r.table_name
-	ORDER BY date_val;
+	AND DATE(partition_description)*1 IS NOT NULL
+	AND (p.table_name = r.table_name OR CONCAT(r.table_name, '_archive') = p.table_name)
+	GROUP BY r.table_name, partition_name, partition_date_id, column_name
+	ORDER BY partition_date_id;
+	
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 	
 	OPEN c_partitions;
 	
 	read_loop: LOOP
-		FETCH c_partitions INTO v_partition, v_table_name, v_archive_name, v_column, v_date_val;
+		FETCH c_partitions INTO v_table_name, v_archive_name, v_partition_name, v_partition_date_id, v_column, v_is_archived, v_is_in_fact;
 		IF done THEN
 		  LEAVE read_loop;
 		END IF;
 		
-		SELECT COUNT(*)
-		INTO v_exists
-		FROM information_schema.PARTITIONS p
-		WHERE p.partition_description = v_date_val
-		AND p.table_name = v_archive_name;
+		SET v_drop_from_archive = 0;
+		SET v_drop_from_fact = 0;
 		
-		IF (v_exists > 0) THEN 
-			SET @s = CONCAT('ALTER TABLE ',v_archive_name,' DROP PARTITION ', v_partition);
-		
-                        PREPARE stmt FROM @s;
+		-- Check if a partition exists in the archive or the fact and is older than the delete policy
+		SELECT v_is_archived, v_is_in_fact
+		INTO v_drop_from_archive, v_drop_from_fact
+		FROM kalturadw_ds.retention_policy
+		WHERE DATE(NOW() - INTERVAL archive_delete_days_back DAY)*1 >= v_partition_date_id
+		AND table_name = v_table_name
+		LIMIT 1;
+	
+		IF (v_drop_from_archive > 0) THEN 
+			SET @s = CONCAT('ALTER TABLE ',v_archive_name,' DROP PARTITION ', v_partition_name);
+			
+			PREPARE stmt FROM @s;
                         EXECUTE stmt;
                         DEALLOCATE PREPARE stmt;
 		END IF;
+
+		-- Check if a partition exists in the fact and is older than the archive policy
+		SELECT COUNT(*)
+		INTO v_migrate_from_fact
+		FROM kalturadw_ds.retention_policy
+		WHERE DATE(NOW() - INTERVAL archive_start_days_back DAY)*1 >= v_partition_date_id
+		AND table_name = v_table_name
+		AND v_is_in_fact > 0;
 		
-		SET @s = CONCAT('ALTER TABLE ',v_archive_name,' ADD PARTITION (PARTITION ',v_partition,' VALUES LESS THAN (',v_date_val,'))');
+		-- If the partition exists in the fact and is older than the archive policy (and not older than the delete policy)
+		IF (v_migrate_from_fact > 0 AND v_drop_from_fact = 0) THEN
+			-- Check if the archive already has a partition convering this time period (drop it)
+			IF (v_is_archived > 0) THEN
+				SET @s = CONCAT('ALTER TABLE ',v_archive_name,' DROP PARTITION ', v_partition_name);
 		
-		PREPARE stmt FROM @s;
-		EXECUTE stmt;
-		DEALLOCATE PREPARE stmt;
-		
-		SET @s = CONCAT('INSERT INTO ',v_archive_name,' SELECT * FROM ',v_table_name,' WHERE ', v_column ,' < ',v_date_val);
-		
-		PREPARE stmt FROM @s;
-		EXECUTE stmt;
-		DEALLOCATE PREPARE stmt;
-		
-		SET @s = CONCAT('ALTER TABLE ',v_table_name,' DROP PARTITION ',v_partition);
-		
-		PREPARE stmt FROM @s;
-		EXECUTE stmt;
-		DEALLOCATE PREPARE stmt;
-		
-		UPDATE kalturadw_ds.retention_policy
-		SET archive_last_partition = DATE(v_date_val)
-		WHERE table_name = v_table_name;
-		
+				PREPARE stmt FROM @s;
+				EXECUTE stmt;
+				DEALLOCATE PREPARE stmt;
+			END IF;
+			
+			-- Migrate it to the archive
+			SET @s = CONCAT('ALTER TABLE ',v_archive_name,' ADD PARTITION (PARTITION ',v_partition_name,' VALUES LESS THAN (',v_partition_date_id,'))');
+			
+			PREPARE stmt FROM @s;
+			EXECUTE stmt;
+			DEALLOCATE PREPARE stmt;
+			
+			SET @s = CONCAT('INSERT INTO ',v_archive_name,' SELECT * FROM ',v_table_name,' WHERE ', v_column ,' < ',v_partition_date_id);
+			
+			PREPARE stmt FROM @s;
+			EXECUTE stmt;
+			DEALLOCATE PREPARE stmt;
+			
+			UPDATE kalturadw_ds.retention_policy
+			SET archive_last_partition = DATE(v_partition_date_id)
+			WHERE table_name = v_table_name;
+
+			SET v_drop_from_fact = 1;
+		END IF;
+
+		-- If partition has migrated from the fact or should be dropped due to the fact that it's older than the delete policy
+		IF (v_drop_from_fact > 0) THEN
+			SET @s = CONCAT('ALTER TABLE ',v_table_name,' DROP PARTITION ',v_partition_name);
+			
+			PREPARE stmt FROM @s;
+			EXECUTE stmt;
+			DEALLOCATE PREPARE stmt;
+		END IF;
 	END LOOP;
-	
 END$$
 
 DELIMITER ;
