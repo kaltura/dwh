1a660c238c525c8cf47a6316bb58575f06e2ad49
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-11-10 12:05:43 +0000

    qnd: hotfixed entry categories updated_at nullable and category longer (333 bytes) - migration scripts
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6851 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/deployed/20111109_6095_split_entry_categories/01.dwh_dim_categories.sql
+++ ddl/migrations/deployed/20111109_6095_split_entry_categories/01.dwh_dim_categories.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS `dwh_dim_categories`;
 
 CREATE TABLE `dwh_dim_categories` (
 	`category_id` INT(11) NOT NULL AUTO_INCREMENT,
-    `category_name` VARCHAR(100) NOT NULL,
+    `category_name` VARCHAR(333) NOT NULL,
 	`dwh_creation_date` TIMESTAMP  NOT NULL DEFAULT '0000-00-00 00:00:00',
 	`dwh_update_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`category_id`), UNIQUE KEY (category_name)
