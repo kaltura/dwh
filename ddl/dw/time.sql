1897bf94da9847d3000e43d45b8dbef48b467972
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-11-13 11:07:26 +0000

    fwr: 1. limited the reset of storage calculation to a variable (with a default value of 90 days) - so that storage wouldn't be recalculated for longer than a desired amount of time. 2. added columns to the time dimensions due to demands by VisionBI. 3. Changed the file assignment to servers according to the server with the least of amount of file size (KB), and not the file count, which is the current algorhythm
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6863 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/dw/time.sql
+++ ddl/dw/time.sql
@@ -1,16 +1,27 @@
-CREATE TABLE `kalturadw`.dwh_dim_time (                        
-	`day_id` INT NOT NULL DEFAULT '0',             
-	`date_field` DATETIME DEFAULT NULL,                
-	`year` INT DEFAULT NULL,                       
-	`month` INT DEFAULT NULL,                      
-	`day_of_year` INT DEFAULT NULL,                
-	`day_of_month` INT DEFAULT NULL,               
-	`day_of_week` INT DEFAULT NULL,                
-	`week_of_year` INT DEFAULT NULL,               
-	`day_of_week_desc` VARCHAR(30) DEFAULT NULL,       
-	`day_of_week_short_desc` VARCHAR(3) DEFAULT NULL,  
-	`month_desc` VARCHAR(30) DEFAULT NULL,             
-	`month_short_desc` VARCHAR(3) DEFAULT NULL,        
-	`quarter` CHAR(1) DEFAULT NULL,                    
-	PRIMARY KEY (`day_id`)                             
-      ) ENGINE=MYISAM DEFAULT CHARSET=latin1
+USE kalturadw;
+
+CREATE TABLE `dwh_dim_time` (
+  `day_id` int(11) NOT NULL DEFAULT '0',
+  `date_field` date NOT NULL,
+  `datetime_field` datetime NOT NULL,
+  `day_eng_name` varchar(50) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `month_str` varchar(50) DEFAULT NULL,
+  `month_id` int(11) DEFAULT NULL,
+  `month_eng_name` varchar(50) DEFAULT NULL,
+  `month` int(11) DEFAULT NULL,
+  `day_of_year` int(11) DEFAULT NULL,
+  `day_of_month` int(11) DEFAULT NULL,
+  `day_of_week` int(11) DEFAULT NULL,
+  `week_id` int(11) DEFAULT NULL,
+  `week_of_year` int(11) DEFAULT NULL,
+  `week_eng_name` varchar(50) DEFAULT NULL,
+  `day_of_week_desc` varchar(30) DEFAULT NULL,
+  `day_of_week_short_desc` varchar(3) DEFAULT NULL,
+  `month_desc` varchar(30) DEFAULT NULL,
+  `month_short_desc` varchar(3) DEFAULT NULL,
+  `quarter` char(1) DEFAULT NULL,
+  `quarter_id` int(11) DEFAULT NULL,
+  `quarter_eng_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`day_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
--- ddl/setup/populate_time_dim.sql
+++ ddl/setup/populate_time_dim.sql
@@ -9,8 +9,13 @@ CREATE DEFINER=`etl`@`localhost` PROCEDURE `populate_time_dim`(start_date dateti
 BEGIN    
 
     WHILE start_date <= end_date DO
-        INSERT IGNORE INTO kalturadw.dwh_dim_time 
-        SELECT 1*DATE(d), d, YEAR(d), MONTH(d), DAYOFYEAR(d),DAYOFMONTH(d),DAYOFWEEK(d),WEEK(d),DAYNAME(d),DATE_FORMAT(d,'%a'),MONTHNAME(d), DATE_FORMAT(d, '%b'), QUARTER(d)
+	INSERT INTO kalturadw.dwh_dim_time 
+	(day_id, date_field, datetime_field, day_eng_name, YEAR, month_str, month_id, month_eng_name, MONTH, day_of_year, day_of_month, 
+	day_of_week, week_id, week_of_year, week_eng_name, day_of_week_desc, day_of_week_short_desc, month_desc, month_short_desc, 
+	QUARTER, quarter_id, quarter_eng_name)
+	SELECT 1*DATE(d), d, d, DATE_FORMAT(d, '%b %e, %Y'), YEAR(d), DATE_FORMAT(d, '%Y-%m'), DATE_FORMAT(d, '%Y%m')*1, DATE_FORMAT(d, '%b-%y'), MONTH(d), DAYOFYEAR(d),DAYOFMONTH(d),
+	DAYOFWEEK(d), DATE_FORMAT(d, '%Y%U')*1, WEEK(d), DATE_FORMAT(d, 'Week %U, %Y'), DAYNAME(d),DATE_FORMAT(d,'%a'),MONTHNAME(d), DATE_FORMAT(d, '%b'), 
+	QUARTER(d), YEAR(d)*10+QUARTER(d), CONCAT('Quarter ', QUARTER(d), ',', YEAR(d))
         FROM(SELECT start_date d) a;
         
         SET start_date = DATE_ADD(start_date, INTERVAL 1 DAY);
