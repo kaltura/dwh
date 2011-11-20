fcae2b4f1932638d6a09537913623ea235d9f18a
omerg <omerg@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-11-20 13:49:40 +0000

    fwr: replacing aggr_storage_mb column with billable_straoge_mb.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6892 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

---
+++ ddl/migrations/development/add_billable_storage_mb_to_partner_usage/07.migrate_storage_data.sql
@@ -0,0 +1,10 @@
+USE `kalturadw`;
+
+INSERT INTO dwh_hourly_partner_usage (date_id, hour_id, partner_id, bandwidth_source_id,  billable_storage_mb)
+SELECT all_time.day_id date_id, partner_id, SUM(count_storage_mb) / DAY(LAST_DAY(date_id)) billable_storage_mb
+FROM dwh_hourly_partner_usage u, dwh_dim_time all_time
+WHERE all_time.day_id <= DATE(NOW())*1 AND all_time.day_id >= date_id
+AND count_storage_mb <> 0
+AND hour_id = 0
+GROUP BY all_time.day_id , partner_id
+ON DUPLICATE KEY UPDATE billable_storage_mb = VALUES(billable_storage_mb);
