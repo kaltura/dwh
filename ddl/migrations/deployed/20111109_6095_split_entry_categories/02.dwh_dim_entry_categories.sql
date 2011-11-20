388c8f57b4c840efbfb4e7155c5b55c721b7683b
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-11-20 13:50:52 +0000

    qnd: hotfixed the update categories of entries process - to not treat to the level
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6893 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/deployed/20111109_6095_split_entry_categories/02.dwh_dim_entry_categories.sql
+++ ddl/migrations/deployed/20111109_6095_split_entry_categories/02.dwh_dim_entry_categories.sql
@@ -12,7 +12,6 @@ create table `dwh_dim_entry_categories` (
 	`partner_id` int(11) NOT NULL,
     	`entry_id` varchar(60) NOT NULL,
         `category_id` int(11) NOT NULL,
-	`level` int(11) NOT NULL,
 	`updated_at` DATETIME,
 	`ri_ind` TINYINT(4)  NOT NULL DEFAULT 0 ,
 	UNIQUE (entry_id, partner_id, `category_id`)
