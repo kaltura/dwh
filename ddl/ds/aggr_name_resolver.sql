5918bf1ecc18d6c7657d538687ff58cb0a56ffca
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-12-01 12:57:46 +0000

    qnd: fixed syntax errors
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6946 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/ds/aggr_name_resolver.sql
+++ ddl/ds/aggr_name_resolver.sql
@@ -36,10 +36,6 @@ values  ('entry','dwh_hourly_events_entry','entry_id','','events'),
 	('api_calls','dwh_hourly_api_calls','action_id', '', 'api'),
 	('errors','dwh_hourly_errors','error_code_id','','errors');
 
-SET     aggr_join_stmt = 'USE INDEX (event_hour_id_event_date_id_partner_id) INNER JOIN kalturadw.dwh_dim_entries AS entry ON(ev.entry_id = entry.entry_id)',
-        aggr_id_field = 'country_id,location_id,os_id,browser_id,ui_conf_id, entry_media_type_id'
-
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
