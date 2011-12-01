9ab5d4bc99fb60f36713f8ea2c8ce35ec328128f
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-12-01 13:32:10 +0000

    qnd: removed the join from the aggr_name_resolver record
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6947 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/deployed/20111201_6111_change_devices_aggregation/04.aggr_name_resolver.sql
+++ ddl/migrations/deployed/20111201_6111_change_devices_aggregation/04.aggr_name_resolver.sql
@@ -1,4 +1,3 @@
 UPDATE kalturadw_ds.aggr_name_resolver
-SET 	aggr_join_stmt = 'USE INDEX (event_hour_id_event_date_id_partner_id) INNER JOIN kalturadw.dwh_dim_entries AS entry ON(ev.entry_id = entry.entry_id)',
-	aggr_id_field = 'country_id,location_id,os_id,browser_id,ui_conf_id, entry_media_type_id'
+SET 	aggr_id_field = 'country_id,location_id,os_id,browser_id,ui_conf_id, entry_media_type_id'
 WHERE aggr_name = 'devices';
