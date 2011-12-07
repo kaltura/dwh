95e1ed838e1ef7b26f8dba0e400dfac2cc96e049
omerg <omerg@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-12-07 16:33:00 +0000

    qnd: fix to ambigous aggr column.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@6971 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/deployed/20111207_6113_add_media_type_join_to_aggr_day/drop_column_aggr_name_resolver.sql
+++ ddl/migrations/deployed/20111207_6113_add_media_type_join_to_aggr_day/drop_column_aggr_name_resolver.sql
@@ -1,3 +1,7 @@
 USE `kalturadw_ds`;
 
 ALTER TABLE aggr_name_resolver DROP COLUMN aggr_join_stmt;
+
+UPDATE aggr_name_resolver
+SET aggr_id_field = 'country_id,location_id,os_id,browser_id,ui_conf_id, e_media_type_id'
+WHERE aggr_name = 'devices';
