39f5a86a1edc698ebbbe286d94066c19012f02ea
dorp <dorp@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2011-12-15 07:48:03 +0000

    qnd: hotfix svn bad auto merge
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7041 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/ds/aggr_name_resolver.sql
+++ ddl/ds/aggr_name_resolver.sql
@@ -14,19 +14,14 @@ USE `kalturadw_ds`;
 CREATE TABLE `aggr_name_resolver` (
   `aggr_name` varchar(100) NOT NULL DEFAULT '',
   `aggr_table` varchar(100) DEFAULT NULL,
-<<<<<<< .mine
   `aggr_id_field` varchar(1024) NOT NULL DEFAULT '',
   `dim_id_field` varchar(1024) NOT NULL DEFAULT '',
-=======
-  `aggr_id_field` varchar(100) DEFAULT NULL,
->>>>>>> .r7039
   `aggr_type` VARCHAR(60) NOT NULL,
   PRIMARY KEY (`aggr_name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 /*Data for the table `aggr_name_resolver` */
 
-<<<<<<< .mine
 insert  into `aggr_name_resolver`(`aggr_name`,`aggr_table`,`aggr_id_field`,`dim_id_field`,`aggr_type`) 
 values  ('entry','dwh_hourly_events_entry','','entry_id','events'),
         ('domain','dwh_hourly_events_domain','domain_id','','events'),
@@ -40,23 +35,3 @@ values  ('entry','dwh_hourly_events_entry','','entry_id','events'),
 		('devices_bandwidth_usage', 'dwh_hourly_events_devices', 'country_id, location_id', '', 'bandwidth'),
 		('api_calls','dwh_hourly_api_calls','action_id', '', 'api'),
 		('errors','dwh_hourly_errors','error_code_id','','errors');
-=======
-insert  into `aggr_name_resolver`(`aggr_name`,`aggr_table`,`aggr_id_field`,`aggr_type`) 
-values  ('entry','dwh_hourly_events_entry','entry_id','events'),
-        ('domain','dwh_hourly_events_domain','domain_id','events'),
-        ('country','dwh_hourly_events_country','country_id,location_id','events'),
-        ('partner','dwh_hourly_partner','','events'),
-        ('widget','dwh_hourly_events_widget','widget_id','events'),
-        ('uid','dwh_hourly_events_uid','kuser_id','events'),
-	('domain_referrer', 'dwh_hourly_events_domain_referrer', 'domain_id, referrer_id', 'events'),
-	('devices', 'dwh_hourly_events_devices', 'country_id,location_id,os_id,browser_id,ui_conf_id, e.entry_media_type_id','events'),
-	('bandwidth_usage', 'dwh_hourly_partner_usage', 'bandwidth_source_id', 'bandwidth'),
-	('devices_bandwidth_usage', 'dwh_hourly_events_devices', 'country_id, location_id', 'bandwidth'),
-	('api_calls','dwh_hourly_api_calls','action_id', 'api'),
-	('errors','dwh_hourly_errors','error_code_id','errors');
->>>>>>> .r7039
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
