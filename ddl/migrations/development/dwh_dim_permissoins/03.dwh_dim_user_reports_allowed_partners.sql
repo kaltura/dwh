783ff1ed9a636664be8db12b4fc6a2bf23c26cbe
svnread <svnread@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2012-04-15 15:34:01 +0000

    qnd: end user level reports
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7445 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

---
+++ ddl/migrations/development/dwh_dim_permissoins/03.dwh_dim_user_reports_allowed_partners.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+USE `kalturadw`$$
+
+DROP VIEW IF EXISTS `dwh_dim_user_reports_allowed_partners`$$
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`etl`@`localhost` SQL SECURITY DEFINER VIEW `dwh_dim_user_reports_allowed_partners` AS (
+SELECT  `dwh_dim_permissions`.`partner_id` AS `partner_id` 
+FROM `dwh_dim_permissions` 
+WHERE ((`dwh_dim_permissions`.`name` = 'END_USER_REPORTS') AND (`dwh_dim_permissions`.`status` = 1)))$$
+
+DELIMITER ;
+
+
+
