3c7404e54ce4ac093e1a6db11039f972464a70f9
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2012-04-23 11:04:08 +0000

    qnd: end user level reports
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7477 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/dw/facts/dwh_fact_events.sql
+++ ddl/dw/facts/dwh_fact_events.sql
@@ -32,7 +32,7 @@ CREATE TABLE kalturadw.dwh_fact_events
 	, browser_id INT(11)
 	, context_id INT(11) DEFAULT NULL
 	, user_id INT(11) DEFAULT NULL
-	,application_id INT(11) DEFAULT NULL,
+	,application_id INT(11) DEFAULT NULL
 	,PRIMARY KEY (file_id,event_id,event_date_id)
 	,KEY Entry_id (Entry_id)
 	,KEY `event_hour_id_event_date_id_partner_id` (event_hour_id, event_date_id, partner_id)
