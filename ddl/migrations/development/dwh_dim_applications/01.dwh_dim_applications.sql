75d277783408ce927f5ab791e36e5933e219f54a
svnread <svnread@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2012-05-06 07:51:38 +0000

    qnd: add partner_id
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7504 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/dwh_dim_applications/01.dwh_dim_applications.sql
+++ ddl/migrations/development/dwh_dim_applications/01.dwh_dim_applications.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS `kalturadw`.`dwh_dim_applications`;
 CREATE TABLE `kalturadw`.`dwh_dim_applications` (
   `application_id` INT AUTO_INCREMENT NOT NULL ,
   `name` VARCHAR(128) DEFAULT 'missing value',
+  `partner_id` INT(11) NOT NULL,
   `dwh_creation_date` TIMESTAMP NOT NULL DEFAULT 0,
   `dwh_update_date` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   `ri_ind` TINYINT NOT NULL DEFAULT '0',
