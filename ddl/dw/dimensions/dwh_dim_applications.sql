ab2dfd2240cbcbe50e46138c6af869cc3216b33c
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2012-07-19 16:17:53 +0000

    qnd: partner_id index
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7791 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/dw/dimensions/dwh_dim_applications.sql
+++ ddl/dw/dimensions/dwh_dim_applications.sql
@@ -3,11 +3,12 @@ DROP TABLE IF EXISTS `kalturadw`.`dwh_dim_applications`;
 CREATE TABLE `kalturadw`.`dwh_dim_applications` (
   `application_id` INT AUTO_INCREMENT NOT NULL ,
   `name` VARCHAR(128) DEFAULT 'missing value',
+  `partner_id` INT(11) NOT NULL,
   `dwh_creation_date` TIMESTAMP NOT NULL DEFAULT 0,
   `dwh_update_date` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   `ri_ind` TINYINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`application_id`),
-  KEY `name_index` (`name`)
+  KEY `partner_id_name_index` (`partner_id`,`name`)
 ) ENGINE=MYISAM  DEFAULT CHARSET=utf8;
 
 CREATE TRIGGER `kalturadw`.`dwh_dim_applications_setcreationtime_oninsert` BEFORE INSERT
