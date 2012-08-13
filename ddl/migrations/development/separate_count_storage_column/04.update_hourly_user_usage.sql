01d77da8c1b36cd53928d6701278ccb1da7b939f
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2012-08-13 17:39:42 +0000

    def: 3931 save added and deleted (storage/entries/msecs) in different columns
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7884 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/separate_count_storage_column/04.update_hourly_user_usage.sql
+++ ddl/migrations/development/separate_count_storage_column/04.update_hourly_user_usage.sql
@@ -1,4 +1,4 @@
-UPDATE kalturadw.dwh_hourly_user_usage SET deleted_storage_mb = IF(added_storage_kb < 0, added_storage_kb, 0), 
+UPDATE kalturadw.dwh_hourly_user_usage SET deleted_storage_kb = IF(added_storage_kb < 0, added_storage_kb, 0), 
                                            deleted_entries = IF(added_entries < 0, added_entries, 0),
 										   deleted_msecs = IF(added_msecs < 0, added_msecs, 0);
 										   
