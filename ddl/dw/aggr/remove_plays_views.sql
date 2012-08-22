4b1e6819bf2ddae81f4533b0a085c0a439932bd8
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2012-08-22 07:42:35 +0000

    qnd: performance fix for remove_plays_views
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7927 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/dw/aggr/remove_plays_views.sql
+++ ddl/dw/aggr/remove_plays_views.sql
@@ -6,12 +6,34 @@ DROP PROCEDURE IF EXISTS `remove_plays_views`$$
 
 CREATE DEFINER=`etl`@`localhost` PROCEDURE `remove_plays_views`(p_date_id INT, p_hour_id INT)
 BEGIN
-	UPDATE kalturadw.dwh_hourly_events_entry h_entry, dwh_entry_plays_views entry_plays_views
-	SET entry_plays_views.plays = 
-		IF((IFNULL(entry_plays_views.plays, 0) - IFNULL(h_entry.count_plays, 0))<0, 
+	  DECLARE v_done INT DEFAULT FALSE;
+  DECLARE v_entry_id VARCHAR(20);
+  DECLARE v_plays INT;
+  
+  DECLARE entries CURSOR FOR SELECT entry_id, count_plays FROM dwh_hourly_events_entry WHERE date_id = p_date_id AND hour_id = p_hour_id;
+  
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_done = TRUE;
+
+  OPEN entries;
+  
+  read_loop: LOOP
+    FETCH entries INTO v_entry_id, v_plays;
+     
+    IF v_done THEN
+      LEAVE read_loop;
+    END IF;
+    
+    UPDATE dwh_entry_plays_views
+	SET plays = 
+		IF((IFNULL(plays, 0) - IFNULL(v_plays, 0))<0, 
 			0, 
-			IFNULL(entry_plays_views.plays, 0) - IFNULL(h_entry.count_plays, 0))
-	WHERE h_entry.entry_id = entry_plays_views.entry_id AND date_id = p_date_id AND hour_id = p_hour_id;
+			IFNULL(plays, 0) - IFNULL(v_plays, 0))
+	WHERE v_entry_id = entry_id;	
+
+  END LOOP;
+
+  CLOSE entries;
+
 END$$
 
 DELIMITER ;
