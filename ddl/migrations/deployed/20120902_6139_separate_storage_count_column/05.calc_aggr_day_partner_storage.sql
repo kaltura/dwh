6979f80077936b12b6840884b43bea770d6041a2
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2012-09-03 10:45:30 +0000

    qnd: fix calc_aggr_day_partner_storage (rollback bandwidth_source_id condition)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@7986 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/deployed/20120902_6139_separate_storage_count_column/05.calc_aggr_day_partner_storage.sql
+++ ddl/migrations/deployed/20120902_6139_separate_storage_count_column/05.calc_aggr_day_partner_storage.sql
@@ -29,7 +29,7 @@ BEGIN
 	FROM        kalturadw.dwh_hourly_partner_usage
 	WHERE       date_id = DATE(date_val - INTERVAL 1 DAY)*1
 	AND         bandwidth_source_id = 1
-	ON DUPLICATE KEY UPDATE count_storage_mb=VALUES(count_storage_mb), aggr_storage_mb = VALUES(aggr_storage_mb);
+	ON DUPLICATE KEY UPDATE added_storage_mb=VALUES(added_storage_mb), deleted_storage_mb=VALUES(deleted_storage_mb), aggr_storage_mb = VALUES(aggr_storage_mb);
 	
 	INSERT INTO 	kalturadw.dwh_hourly_partner_usage (partner_id, date_id, hour_id, bandwidth_source_id, added_storage_mb, deleted_storage_mb, aggr_storage_mb)
 	SELECT		aggr.partner_id, aggr.date_id, aggr.hour_id, 1, aggr.added_storage_mb, aggr.deleted_storage_mb, aggr.added_storage_mb - aggr.deleted_storage_mb
