2d68a0f89c3268fcf7b48ca0fd62de0fe06b387d
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2013-01-24 12:16:44 +0000

    qnd: ingestion reports changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@8196 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

---
+++ ddl/migrations/development/ingestion_aggr/calc_aggr_day_ingestion.sql
@@ -0,0 +1,101 @@
+DELIMITER $$
+
+USE `kalturadw`$$
+
+DROP PROCEDURE IF EXISTS `calc_aggr_day_ingestion`$$
+
+CREATE DEFINER=`etl`@`localhost` PROCEDURE `calc_aggr_day_ingestion`(p_date_id INT(11))
+BEGIN
+	DECLARE v_ignore DATE;
+	DECLARE v_from_archive DATE;
+        DECLARE v_table_name VARCHAR(100);
+	DECLARE v_aggr_table VARCHAR(100);
+	DECLARE v_aggr_id_field_str VARCHAR(100);
+	
+	UPDATE aggr_managment SET start_time = NOW() WHERE aggr_name = 'ingestion' AND date_id = p_date_id;
+		
+	            
+	SET @s = CONCAT('INSERT INTO kalturadw.dwh_daily_ingestion (date_id, normal_wait_time_count, medium_wait_time_count, long_wait_time_count, extremely_long_wait_time_count,stuck_wait_time_count)'
+					'SELECT created_date_id, COUNT(IF(wait_time< 5,1,null)) normal_wait_time, COUNT(IF(wait_time>5 AND wait_time < 180,1,null)) medium_wait_time, COUNT(IF(wait_time>180 AND wait_time<900,1,null)) long_wait_time,
+					COUNT(IF(wait_time>900 AND wait_time < 3600,1,null)) extremely_long_wait_time_count, COUNT(IF(wait_time>3600)) stuck
+					FROM kalturadw.dwh_fact_convert_job
+					WHERE is_ff = 1 
+					AND created_date_id = ' ,p_date_id , 
+					'ON DUPLICATE KEY UPDATE	
+						normal_wait_time_count=VALUES(normal_wait_time_count),
+						medium_wait_time_count=VALUES(medium_wait_time_count),
+						long_wait_time_count=VALUES(long_wait_time_count),
+						extremely_long_wait_time_count=VALUES(extremely_long_wait_time_count),
+						stuck_wait_time_count=VALUES(stuck_wait_time_count);');
+	
+		
+	PREPARE stmt FROM  @s;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+	
+	SET @s = CONCAT('INSERT INTO kalturadw.dwh_daily_ingestion (date_id, success_entries_count, failed_entries_count)'
+					'SELECT COUNT(IF(entry.status_id=2, 1, NULL) entries_success, COUNT(IF(entry.status_id=-1, 1, NULL) entries_failure
+					FROM kalturadw.dwh_dim_entries entry, kalturadw.dwh_dim_batch_job_sep job
+					WHERE entry.entry_id = job.entry_id
+					AND entry.created_date_id = ' , p_date_id,
+					'AND e.entry_media_type_id IN (1,5)
+					ON DUPLICATE KEY UPDATE	
+						success_entries_count=VALUES(success_entries_count),
+						failed_entries_count=VALUES(failed_entries_count);');
+	
+		
+	PREPARE stmt FROM  @s;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+	
+	SET @s = CONCAT('INSERT INTO kalturadw.dwh_daily_ingestion (date_id, success_conversion_job_count, failed_conversion_job_count)'
+					'SELECT created_date_id, COUNT(IF(status_id=5,1,NULL)) conversion_job_success, COUNT(IF(status_id=6 OR status_id = 10,1,NULL)) conversion_job_failure 
+					 FROM kalturadw.dwh_fact_convert_job
+					 WHERE created_date_id = ', p_date_id,
+					 'ON DUPLICATE KEY UPDATE	
+						success_conversion_job_count=VALUES(success_conversion_job_count),
+						failed_conversion_job_count=VALUES(failed_conversion_job_count);');
+	
+		
+	PREPARE stmt FROM  @s;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+			
+	SET @s = CONCAT('INSERT INTO kalturadw.dwh_daily_ingestion (date_id, avg_wait_time)'
+					'SELECT created_date_id, AVG(wait_time)
+					 FROM kalturadw.dwh_fact_convert_job
+					 WHERE created_date_id = ' ,p_date_id,
+					 'ON DUPLICATE KEY UPDATE	
+						avg_wait_time=VALUES(avg_wait_time);');
+	
+		
+	PREPARE stmt FROM  @s;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+		/* FMS */
+		SELECT DATE(archive_last_partition)
+		INTO v_from_archive
+		FROM kalturadw_ds.retention_policy
+		WHERE table_name = 'dwh_fact_fms_sessions';
+		
+		IF (p_date_val >= v_from_archive) THEN 
+                        SET v_table_name = 'dwh_fact_fms_sessions';
+                ELSE
+                        SET v_table_name = 'dwh_fact_fms_sessions_archive';
+                END IF;
+
+		SET @s = CONCAT('INSERT INTO kalturadw.', v_aggr_table, ' (partner_id, date_id, hour_id', v_aggr_id_field_str,', count_bandwidth_kb)
+				SELECT session_partner_id, MAX(session_date_id), 0 hour_id', v_aggr_id_field_str,', SUM(total_bytes)/1024 count_bandwidth 
+				FROM ', v_table_name, ' WHERE session_date_id=date(\'',p_date_val,'\')*1
+				GROUP BY session_partner_id', v_aggr_id_field_str,'
+				ON DUPLICATE KEY UPDATE	count_bandwidth_kb=VALUES(count_bandwidth_kb)');
+		PREPARE stmt FROM  @s;
+                EXECUTE stmt;
+                DEALLOCATE PREPARE stmt;
+	
+	END IF;
+	
+	UPDATE aggr_managment SET end_time = NOW() WHERE aggr_name = p_aggr_name AND date_id = DATE(p_date_val)*1;
+END$$
+
+DELIMITER ;
