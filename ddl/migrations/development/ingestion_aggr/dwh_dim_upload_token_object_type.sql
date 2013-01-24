c82d09a45bcdc1ec86c39fa96f7bcbfa20e72846
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2013-01-24 14:07:39 +0000

    qnd: ingestion reports changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@8209 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/ingestion_aggr/dwh_dim_upload_token_object_type.sql
+++ ddl/migrations/development/ingestion_aggr/dwh_dim_upload_token_object_type.sql
@@ -1,7 +1,7 @@
 USE kalturadw;
 
 DROP TABLE IF EXISTS `dwh_dim_upload_token_object_type`;
-CREATE TABLE `dwh_dim_browser` (
+CREATE TABLE `dwh_dim_upload_token_object_type` (
   `upload_token_object_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `upload_token_object_type_name` varchar(127) NOT NULL,
   PRIMARY KEY (`upload_token_object_type_id`),
