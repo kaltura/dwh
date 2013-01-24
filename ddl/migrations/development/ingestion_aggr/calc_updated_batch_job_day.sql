819fd2c40a3706b86d8ae14bec8a04f028e8a407
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2013-01-24 13:56:38 +0000

    qnd: ingestion reports changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@8202 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/ingestion_aggr/calc_updated_batch_job_day.sql
+++ ddl/migrations/development/ingestion_aggr/calc_updated_batch_job_day.sql
@@ -11,12 +11,12 @@ BEGIN
                 SET v_ignore_partner_ids = '';
 				
                 
-                SELECT group_concat(partner_id)
+                SELECT group_concat(ignore_partner.partner_id)
 				INTO v_ignore_partner_ids
 				FROM 
 				(SELECT partner_id FROM kalturadw.dwh_dim_batch_job_sep WHERE job_type in (1,2,3,99) AND updated_date_id = p_date_id
 				GROUP BY partner_id
-				HAVING COUNT(*) > 10000);
+				HAVING COUNT(*) > 10000) ignore_partner;
                                 
                 v_ignore_partner_ids = LEFT(v_ignore_partner_ids,LENGTH(v_ignore_partner_ids)-1);
                                 
@@ -35,12 +35,12 @@ BEGIN
 				EXECUTE stmt;
 				DEALLOCATE PREPARE stmt;	
 					
-				SELECT group_concat(partner_id)
+				SELECT group_concat(ignore_partner.partner_id)
 				INTO v_ignore_partner_ids
 				FROM 
 				(SELECT partner_id FROM kalturadw.dwh_dim_batch_job_sep WHERE job_type = 10 AND updated_date_id = p_date_id
 				GROUP BY partner_id
-				HAVING COUNT(*) > 10000);
+				HAVING COUNT(*) > 10000) ignore_partner;
 				
                 SET @s = CONCAT("INSERT INTO kalturadw.dwh_fact_convert_job(id, job_type_id, stauts_id, created_date_id, updated_date_id, finish_date_id, partner_id, dc, wait_time, is_ff
                 SELECT id, job_type_id, stauts_id, created_date_id, updated_date_id, finish_date_id, partner_id, dc, time_to_sec(timediff(queue_time, create_at) wait_time, IFNULL(finish_time, -1, time_to_sec(finish_time, queue_time)) conversion_time, 1 
