ae97ce80bb705881938499ff4171bbd039af7bdb
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2013-02-03 09:33:29 +0000

    qnd: ingestion reports changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@8223 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/migrations/development/ingestion_aggr/13.dwh_daily_ingestion.sql
+++ ddl/migrations/development/ingestion_aggr/13.dwh_daily_ingestion.sql
@@ -1,17 +1,3 @@
-/*
-SQLyog Community v8.7 
-MySQL - 5.1.37-log : Database - kaltura
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 USE `kalturadw`;
 
 DROP TABLE IF EXISTS `dwh_daily_ingestion`;
@@ -36,9 +22,8 @@ CREATE TABLE `dwh_daily_ingestion` (
   `median_ff_wait_time_sec` bigint(22) DEFAULT '0',
   PRIMARY KEY (`date_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-/*!50100 PARTITION BY RANGE (date_id)
-(PARTITION p_201301 VALUES LESS THAN (20130201) ENGINE = InnoDB) */
+PARTITION BY RANGE (date_id)
+(PARTITION p_201301 VALUES LESS THAN (20130201) ENGINE = InnoDB);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CALL kalturadw.add_monthly_partition_for_table('dwh_hourly_events_context_app');
+
