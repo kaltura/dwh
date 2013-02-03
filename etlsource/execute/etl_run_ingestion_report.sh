eb4a08d5f3bf9c08a3b7314e89f454a484fd19a9
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2013-02-03 10:57:43 +0000

    qnd: ingestion reports changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@8236 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

---
+++ etlsource/execute/etl_run_ingestion_report.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ROOT_DIR=/opt/kaltura/dwh
+MAIL_SUBJECT="Daily Usage Report"
+RECIPIENTS="orly.lampert@kaltura.com"
+ATTACH=/tmp/ingestion_dashboard.pdf
+REPORT=ingestion_monthly.rptdesign
+OUT=PDF
+STARTDATE=$(date --date '1 month ago' +%Y%m01)
+
+while getopts "s:r:f:o:a:q:" o
+do      case "$o" in
+    s)  MAIL_SUBJECT="$OPTARG";;
+    r)  RECIPIENTS="$OPTARG";;
+    a)  ATTACH="$OPTARG";;
+    f)  REPORT="$OPTARG";;
+    o)  OUT="$OPTARG";;
+    q)  PREEMPTIVE_SQL_CHECK_FILE="$OPTARG";;
+        [?])    echo >&2 "Usage: $0 [-s mail_subject] [-r recipients,comma-seperated] [-f report-file-name] [-o output-format PDF|XLS] [-a attachment-name]"
+                exit 1;;
+        esac
+done
+
+res=0
+        export BIRT_HOME=/home/kaltura/birt-runtime-4_2_1/
+        sh $BIRT_HOME/ReportEngine/genReport.sh -m runrender -f $OUT -p "DateIDParam=20121201" -o $ATTACH $ROOT_DIR/reports/$REPORT 
+
