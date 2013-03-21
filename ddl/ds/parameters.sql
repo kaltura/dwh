6ddc1f8bf37ded5473a3a0f7191919113bcee460
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2013-03-21 15:34:10 +0000

    qnd: ingestion reports changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@8345 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- ddl/ds/parameters.sql
+++ ddl/ds/parameters.sql
@@ -12,7 +12,7 @@ INSERT INTO kalturadw_ds.parameters (id, process_id, parameter_name, int_value, 
 (2, 0, "dim_sync_last_update", -1, NOW() - INTERVAL 1 DAY),
 (3, 2, 'fms_stale_session_days_limit', 3, null),
 (8, 0, 'referencial_integrity_last_update', 0, date(20110101)),
-(10, 'convert_job_fact_last_update', 0, NOW() - INTERVAL 1 DAY);
+(10, 0, 'convert_job_fact_last_update', 0, NOW() - INTERVAL 1 DAY);
 
 INSERT kalturadw_ds.parameters (id, parameter_name)
 VALUES (4, 'sync_last_execution_plays_views'),
