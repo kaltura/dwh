febde9834a3f6050bf6db40ef2c70a679efaf9fc
orlyl <orlyl@09e43ab6-4768-4770-81d3-4e3a7c8fdb74>
2013-05-01 09:58:03 +0000

    qnd: preserve owner, group & permissions for libext jars
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalprod/dwh/core/trunk@8467 09e43ab6-4768-4770-81d3-4e3a7c8fdb74

--- setup/copy_pentaho_plugins.sh
+++ setup/copy_pentaho_plugins.sh
@@ -17,5 +17,5 @@ rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/MappingFieldRunner42/MappingF
 rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/GetFTPFileNames42/GetFTPFileNames $KITCHEN/plugins/steps/
 rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/FetchFTPFile42/FetchFTPFile $KITCHEN/plugins/steps/
 rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/DimLookup42/DimLookup $KITCHEN/plugins/steps/
-rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/resources/* $KITCHEN/libext/
+rsync -avpog --exclude=.svn $ROOT_DIR/pentaho-plugins/resources/* $KITCHEN/libext/
 chown -R kaltura.kaltura $KITCHEN/plugins
