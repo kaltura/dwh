8e368daeec51534b86688b414657652c63023556
Orly Lampert <orly.lampert@kaltura.com>
2014-06-23 12:47:51 +0300

    PLAT-1070: add fact_plays table to retention policy

--- ddl/ds/retention_policy.sql
+++ ddl/ds/retention_policy.sql
@@ -10,11 +10,11 @@ CREATE TABLE retention_policy (
 ENGINE=MYISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO retention_policy VALUES 
-('dwh_fact_events', 30, 2000, DATE('2011-01-01')),
-('dwh_fact_bandwidth_usage', 30, 2000, DATE('2011-01-01')),
-('dwh_fact_fms_session_events', 30, 2000, DATE('2011-01-01')),
-('dwh_fact_fms_sessions', 30, 2000, DATE('2011-01-01')),
-('dwh_fact_api_calls', 30, 2000, DATE('2011-01-01')),
+('dwh_fact_events', 30, 365, DATE('2011-01-01')),
+('dwh_fact_bandwidth_usage', 30, 365, DATE('2011-01-01')),
+('dwh_fact_fms_session_events', 30, 365, DATE('2011-01-01')),
+('dwh_fact_fms_sessions', 30, 365, DATE('2011-01-01')),
+('dwh_fact_api_calls', 30, 365, DATE('2011-01-01')),
 ('dwh_fact_incomplete_api_calls', 30, 3, NULL),
-('dwh_fact_errors', 365, 2000, DATE(20110101)),
+('dwh_fact_errors', 365, 365, DATE(20110101)),
 ('dwh_fact_plays', 30, 365, DATE('2014-01-01'));
