8d7007ed9e14b6067e0065d0ba6a50fd91e61cba
Orly Lampert <orly.lampert@kaltura.com>
2014-06-24 17:11:52 +0300

    Upgrade scripts + kaltura live analytics

--- ddl/ds/populate_repository_for_bandwidth_usage.sql
+++ ddl/ds/populate_repository_for_bandwidth_usage.sql
@@ -2,6 +2,7 @@
 INSERT INTO kalturadw_ds.processes (id, process_name, max_files_per_cycle) VALUE (5, 'bandwidth_usage_LLN', 50);
 INSERT INTO kalturadw_ds.processes (id, process_name, max_files_per_cycle) VALUE (6, 'bandwidth_usage_LEVEL3', 1000);
 INSERT INTO kalturadw_ds.processes (id, process_name, max_files_per_cycle) VALUE (10, 'bandwidth_usage_AKAMAI_LIVE_URTMP', 50);
+INSERT INTO kalturadw_ds.processes (id, process_name, max_files_per_cycle) VALUE (12, 'kaltura_live_bandwidth', 20);
 
 INSERT INTO `kalturadw_ds`.`staging_areas`
         (`id`,
@@ -124,3 +125,25 @@ INSERT INTO `kalturadw_ds`.`staging_areas`
 	'activity_date_id',
 	'activity_hour_id'
         );
+		
+INSERT INTO kalturadw_ds.staging_areas
+        (id,
+        process_id,
+        source_table,
+        target_table_id,
+        on_duplicate_clause,
+        staging_partition_field,
+        post_transfer_sp,
+                post_transfer_aggregations,
+                aggr_date_field,
+                hour_id_field)
+VALUES
+        (14,      12,
+         'ds_bandwidth_usage',
+         2,
+         NULL,
+         'cycle_id',
+         NULL,
+        '(\'bandwidth_usage\')',
+        'activity_date_id',
+        'activity_hour_id');
---
+++ ddl/dw/aggr/post_aggregation_live_entry.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+USE `kalturadw`$$
+
+DROP PROCEDURE IF EXISTS `post_aggregation_live_entry`$$
+
+CREATE PROCEDURE `post_aggregation_live_entry`(date_val DATE, p_hour_id INT(11))
+BEGIN
+        CALL add_live_plays_views(date_val*1, p_hour_id);
+
+END$$
+
+DELIMITER ;
