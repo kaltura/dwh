4e61727501cd1a2478d0cdc92f9b7b424f21f156
Orly Lampert <orly.lampert@kaltura.com>
2014-06-25 09:33:54 +0300

    fix syntax errors in upgrade script

--- ddl/migrations/20140623_Hercules_to_Iris/hercules2Iris.sql
+++ ddl/migrations/20140623_Hercules_to_Iris/hercules2Iris.sql
@@ -278,8 +278,8 @@ CREATE TABLE `dwh_fact_plays_archive` (
   `os_id` int(11),
   `browser_id` int(11)
 ) ENGINE=ARCHIVE DEFAULT CHARSET=utf8
-/*!50100 PARTITION BY RANGE (play_date_id)
-(PARTITION p_0 VALUES LESS THAN (1) ENGINE = ARCHIVE)*/
+PARTITION BY RANGE (play_date_id)
+(PARTITION p_0 VALUES LESS THAN (1) ENGINE = ARCHIVE);
 
 
 DELIMITER $$
