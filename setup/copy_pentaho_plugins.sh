911e06b0e5877f3e32a1362c17cb427a65675682
Orly Lampert <orly.lampert@kaltura.com>
2014-07-20 09:14:08 +0300

    add upgrade script to copy pentaho plugins

--- setup/copy_pentaho_plugins.sh
+++ setup/copy_pentaho_plugins.sh
@@ -13,11 +13,11 @@ do      case "$o" in
         esac
 done
 
-rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/MySQLInserter42/MySQLInserter $KITCHEN/plugins/steps/
-rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/MappingFieldRunner42/MappingFieldRunner $KITCHEN/plugins/steps/
-rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/GetFTPFileNames42/GetFTPFileNames $KITCHEN/plugins/steps/
-rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/FetchFTPFile42/FetchFTPFile $KITCHEN/plugins/steps/
-rsync -av --exclude=.svn $ROOT_DIR/pentaho-plugins/DimLookup42/DimLookup $KITCHEN/plugins/steps/
+rsync -av $ROOT_DIR/pentaho-plugins/MySQLInserter42/MySQLInserter $KITCHEN/plugins/steps/
+rsync -av $ROOT_DIR/pentaho-plugins/MappingFieldRunner42/MappingFieldRunner $KITCHEN/plugins/steps/
+rsync -av $ROOT_DIR/pentaho-plugins/GetFTPFileNames42/GetFTPFileNames $KITCHEN/plugins/steps/
+rsync -av $ROOT_DIR/pentaho-plugins/FetchFTPFile42/FetchFTPFile $KITCHEN/plugins/steps/
+rsync -av $ROOT_DIR/pentaho-plugins/DimLookup42/DimLookup $KITCHEN/plugins/steps/
 chown -R kaltura.kaltura $ROOT_DIR/pentaho-plugins/resources
-rsync -avpog --exclude=.svn $ROOT_DIR/pentaho-plugins/resources/* $KITCHEN/libext/
+rsync -avpog $ROOT_DIR/pentaho-plugins/resources/* $KITCHEN/libext/
 chown -R kaltura.kaltura $KITCHEN/plugins
